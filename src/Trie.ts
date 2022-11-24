63b07a52c673cebc56b46ef950c31baa56310c21
Nail Gafarov <nil20122013@gmail.com>
2022-11-24 20:04:20 +0500

    Basic Trie structure

--- src/Trie.ts
+++ src/Trie.ts
@@ -1,2 +1,29 @@
+// TODO: compressing
 // TODO
-export default class Trie {}
+export default class Trie {
+
+	// TODO: Should it be replaced with Map with custom generic type?
+	private __children: {[key: string]: Trie} = {};
+	private __parent: Trie | null = null;
+	private __end: boolean = false;
+
+	public constructor() {}
+
+	public hasPrefix(prefix: string, exact: boolean = false): boolean {}
+
+	public getPrefix(prefix: string, exact: boolean = false): Trie | null {}
+
+	public addPrefix(prefix: string, exact: boolean = false): void {}
+
+	public removePrefix(prefix: string, exact: boolean = false) {}
+
+	public getParent(): Trie | null {}
+
+	public toArray(): string[] {}
+
+	public forEach(f: (prefix: string) => void): void {} // TODO: Replace with iterator
+
+	public length(): number {}
+	
+	public static fromArray(data: string): Trie {}
+}
