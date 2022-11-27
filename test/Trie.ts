7a9999d5b9a1b3e1cdb9fbb42ebd0f1d309ed641
Nail Gafarov <nil20122013@gmail.com>
2022-11-27 18:50:40 +0500

    Removed boilerplate

--- test/Trie.ts
+++ test/Trie.ts
@@ -9,37 +9,37 @@ mocha.describe("Trie", () => {
 			assert.equal(Trie.create().length, 0);
 		});
 		mocha.it("Should decrease after removing an existing prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("First");
 			assert.equal(t.length, 2);
 		});
 		mocha.it("Should be the same after an attempt to remove non-existing prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("Fourth");
 			assert.equal(t.length, 3);
 		});
 		mocha.it("Should be the same after an attempt to remove partial (not exact) prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("Firs");
 			assert.equal(t.length, 3);
 		});
 		mocha.it("Should increase after adding a prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.addPrefix("Fourth");
 			assert.equal(t.length, 4);
 		});
 		mocha.it("Should increase after an adding existing non-final prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.addPrefix("Fi");
 			assert.equal(t.length, 4);
 		});
 		mocha.it("Should be the same after an attempt to add an existing prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.addPrefix("First");
 			assert.equal(t.length, 3);
 		});
 		mocha.it("Should be the same after removing an empty string if the trie does not contain an empty one", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("");
 			assert.equal(t.length, 3);
 		});
@@ -50,152 +50,94 @@ mocha.describe("Trie", () => {
 			assert.equal(t.length, 3);
 		});
 		mocha.it("Should increase after adding an empty string", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			assert.equal(t.length, 3);
 			t.addPrefix("");
 			assert.equal(t.length, 4);
 		});
 		mocha.it("Should return correct number", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			assert.equal(t.length, 3);
 		});
 	});
 	mocha.describe("*[Symbol.iterator]()", () => {
 		mocha.it("The loop should be empty when a trie is empty", () => {
 			const t = Trie.fromArray([]);
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 0);
 			assert.deepStrictEqual(array, []);
 		});
 		mocha.it("The loop iterations count should be decreased after removing an existing prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("First");
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 2);
 			assert.deepStrictEqual(array, ["Second", "Third"]);
 		});
 		mocha.it("The loop should have the same amount of iterations after an attempt to remove non-existing prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("Fourth");
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 3);
 			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
 		});
 		mocha.it("The loop should have the same amount of iterations after an attempt to remove partial (not exact) prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.removePrefix("Firs");
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 3);
 			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
 		});
 		mocha.it("The loop should increase the amount of iterations after adding a prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.addPrefix("Fourth");
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 4);
 			assert.deepStrictEqual(array, ["First", "Fourth", "Second", "Third"]);
 		});
 		mocha.it("The loop should increase the amount of iterations after adding an existing non-final prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.addPrefix("Fi");
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 4);
 			assert.deepStrictEqual(array, ["Fi", "First", "Second", "Third"]);
 		});
 		mocha.it("The loop should have the same amount of iterations after an attempt to add an existing prefix", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
+			const t = create();
 			t.addPrefix("First");
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 3);
 			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
 		});
 		mocha.it("The loop should iterate through a trie in alphabetical order", () => {
 			const t = Trie.fromArray(["ghi", "def", "abc"]);
-			let array: string[] = [];
-			for (const word of t) {
-				array.push(word);
-			}
+			const [, array] = iterate(t);
 			assert.deepStrictEqual(array, ["abc", "def", "ghi"]);
 		});
 		mocha.it("The loop should have an expected amount of iterations", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
-			let i = 0;
-			let array: string[] = [];
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const t = create();
+			const [i, array] = iterate(t);
 			assert.equal(i, 3);
 			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
 		});
 		mocha.it("The loop should have the same amount of iterations after removing an empty string if the trie does not contain an empty one", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
-			let i = 0;
-			let array: string[] = [];
+			const t = create();
 			t.removePrefix("");
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 3);
 			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
 		});
 		mocha.it("The loop should should decrease the amount of iterations after removing an empty string if the trie contain an empty one", () => {
 			const t = Trie.fromArray(["First", "Second", "Third", ""]);
-			let i = 0;
-			let array: string[] = [];
 			t.removePrefix("");
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 3);
 			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
 		});
 		mocha.it("The loop should should increase the amount of iterations after adding an empty string", () => {
-			const t = Trie.fromArray(["First", "Second", "Third"]);
-			let i = 0;
-			let array: string[] = [];
+			const t = create();
 			t.addPrefix("");
-			for (const word of t) {
-				i++;
-				array.push(word);
-			}
+			const [i, array] = iterate(t);
 			assert.equal(i, 4);
 			assert.deepStrictEqual(array, ["", "First", "Second", "Third"]);
 		});
@@ -217,3 +159,17 @@ mocha.describe("Trie", () => {
 	// TODO
 	mocha.describe("Data of the children", () => {});
 });
+
+function create(): Trie {
+	return Trie.fromArray(["First", "Second", "Third"]);
+}
+
+function iterate(trie: Trie): [number, string[]] {
+	let i = 0;
+	let result: string[] = [];
+	for (const word of trie) {
+		i++;
+		result.push(word);
+	}
+	return [i, result];
+}
