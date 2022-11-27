1527b89ce2afa4bb281f0396341712648b7010b1
Nail Gafarov <nil20122013@gmail.com>
2022-11-27 18:21:45 +0500

    Added tests for *[Symbol.iterator]() method

--- test/Trie.ts
+++ test/Trie.ts
@@ -3,7 +3,6 @@ import * as mocha from "mocha";
 import Trie from "../src/Trie";
 
 mocha.describe("Trie", () => {
-	// TODO
 	mocha.describe("get length()", () => {
 		mocha.it("Should return 0 for empty trie", () => {
 			assert.equal(Trie.fromArray([]).length, 0);
@@ -44,8 +43,110 @@ mocha.describe("Trie", () => {
 			assert.equal(t.length, 3);
 		});
 	});
-	// TODO
-	mocha.describe("*[Symbol.iterator]()", () => {});
+	mocha.describe("*[Symbol.iterator]()", () => {
+		mocha.it("The loop should be empty when a trie is empty", () => {
+			const t = Trie.fromArray([]);
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 0);
+			assert.deepStrictEqual(array, []);
+		});
+		mocha.it("The loop iterations count should be decreased after removing an existing prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.removePrefix("First");
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 2);
+			assert.deepStrictEqual(array, ["Second", "Third"]);
+		});
+		mocha.it("The loop should have the same amount of iterations after an attempt to remove non-existing prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.removePrefix("Fourth");
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 3);
+			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
+		});
+		mocha.it("The loop should have the same amount of iterations after an attempt to remove partial (not exact) prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.removePrefix("Firs");
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 3);
+			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
+		});
+		mocha.it("The loop should increase the amount of iterations after adding a prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.addPrefix("Fourth");
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 4);
+			assert.deepStrictEqual(array, ["First", "Fourth", "Second", "Third"]);
+		});
+		mocha.it("The loop should increase the amount of iterations after adding an existing non-final prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.addPrefix("Fi");
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 4);
+			assert.deepStrictEqual(array, ["Fi", "First", "Second", "Third"]);
+		});
+		mocha.it("The loop should have the same amount of iterations after an attempt to add an existing prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.addPrefix("First");
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 3);
+			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
+		});
+		mocha.it("The loop should iterate through a trie in alphabetical order", () => {
+			const t = Trie.fromArray(["ghi", "def", "abc"]);
+			let array: string[] = [];
+			for (const word of t) {
+				array.push(word);
+			}
+			assert.deepStrictEqual(array, ["abc", "def", "ghi"]);
+		});
+		mocha.it("The loop should have an expected amount of iterations", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			let i = 0;
+			let array: string[] = [];
+			for (const word of t) {
+				i++;
+				array.push(word);
+			}
+			assert.equal(i, 3);
+			assert.deepStrictEqual(array, ["First", "Second", "Third"]);
+		});
+	});
 	// TODO
 	mocha.describe("hasPrefix()", () => {});
 	// TODO
