b19b93fc09d29527945eb67b2d2d616814b6caa5
Nail Gafarov <nil20122013@gmail.com>
2022-11-27 18:00:56 +0500

    Added tests for length() getter

--- test/Trie.ts
+++ test/Trie.ts
@@ -4,7 +4,46 @@ import Trie from "../src/Trie";
 
 mocha.describe("Trie", () => {
 	// TODO
-	mocha.describe("length()", () => {});
+	mocha.describe("get length()", () => {
+		mocha.it("Should return 0 for empty trie", () => {
+			assert.equal(Trie.fromArray([]).length, 0);
+			assert.equal(Trie.create().length, 0);
+		});
+		mocha.it("Should decrease after removing an existing prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.removePrefix("First");
+			assert.equal(t.length, 2);
+		});
+		mocha.it("Should be the same after an attemt to remove non-existing prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.removePrefix("Fourth");
+			assert.equal(t.length, 3);
+		});
+		mocha.it("Should be the same after an attemt to remove partial (not exact) prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.removePrefix("Firs");
+			assert.equal(t.length, 3);
+		});
+		mocha.it("Should increase after adding a prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.addPrefix("Fourth");
+			assert.equal(t.length, 4);
+		});
+		mocha.it("Should be the same after an adding existing non-final prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.addPrefix("Fi");
+			assert.equal(t.length, 4);
+		});
+		mocha.it("Should be the same after an attemt to add an existing prefix", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			t.addPrefix("First");
+			assert.equal(t.length, 3);
+		});
+		mocha.it("Should return correct number", () => {
+			const t = Trie.fromArray(["First", "Second", "Third"]);
+			assert.equal(t.length, 3);
+		});
+	});
 	// TODO
 	mocha.describe("*[Symbol.iterator]()", () => {});
 	// TODO
@@ -23,4 +62,6 @@ mocha.describe("Trie", () => {
 	mocha.describe("fromArray()", () => {});
 	// TODO
 	mocha.describe("create()", () => {});
+	// TODO
+	mocha.describe("Data of the children", () => {});
 });
